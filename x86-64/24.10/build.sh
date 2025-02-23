312b894423dd3923784bd12a7226aa883ac896f1
qzshrx <81845033+qzshrx@users.noreply.github.com>
2025-02-23 22:58:05 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -29,6 +29,7 @@ PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-app-airplay2"
 #支持计划重启
 PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
@@ -40,7 +41,7 @@ PACKAGES="$PACKAGES znc-mod-fail2ban"
 #KMS服务器
 PACKAGES="$PACKAGES luci-app-vlmcsd"
 #流量监控工具
-PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 #Client-Splash是无线MESH网络的一个热点认证系统
 #PACKAGES="$PACKAGES luci-i18n-splash-zh-cn"
 #BATMAN-adv协议软件包，用于在mesh网络中实现路由器之间的通信
@@ -50,9 +51,9 @@ PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 #分布式AP管理程序
 #PACKAGES="$PACKAGES luci-i18n-dawn-zh-cn"
 #ARP 绑定工具，可以将 IP 地址绑定到设备 MAC 地址上，防止 IP 地址被冒用
-PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
 #一个抓包分析工具，用于网络监测和故障排除
-PACKAGES="$PACKAGES luci-i18n-cshark-zh-cn"	
+#PACKAGES="$PACKAGES luci-i18n-cshark-zh-cn"	
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
@@ -65,16 +66,16 @@ PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 #带宽控制，用于限制每个设备的带宽使用
-PACKAGES="$PACKAGES luci-i18n-xlnetacc-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-xlnetacc-zh-cn"
 #允许对网络访问进行控制，例如阻止某些设备访问互联网,not
-PACKAGES="$PACKAGES luci-app-accesscontrol"
+#PACKAGES="$PACKAGES luci-app-accesscontrol"
 #具有广告过滤、隐私保护、家长控制等功能的 DNS 服务器
 PACKAGES="$PACKAGES luci-app-adguardhome"
 #防止IP欺诈攻击的插件，可以实现基于BCP 38规范的反欺诈功能
-PACKAGES="$PACKAGES luci-app-bcp38"
+#PACKAGES="$PACKAGES luci-app-bcp38"
 #软件市场，not
 #PACKAGES="$PACKAGES luci-app-store"
-PACKAGES="$PACKAGES "
+#PACKAGES="$PACKAGES "
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
